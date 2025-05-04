0fea6f48dc3d8ff1aa77f36c25fdbb82adb99272
shabalin13 <d.shabal1n12@gmail.com>
2025-05-04 15:59:49 +0300

    Add and configure detekt library

--- app/src/main/java/com/shabalin13/testci/MainActivity.kt
+++ app/src/main/java/com/shabalin13/testci/MainActivity.kt
@@ -8,17 +8,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.shabalin13.testci.ui.theme.TestCITheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TestCITheme {
+            TestCITheme{
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Text(
                         text = "Hello, Dmitrii!",
